2d9cec6f047c1c1dbfdc1f59cfa449857b8a29ce
Fu An Yang <60484800+soda00000@users.noreply.github.com>
2021-05-26 15:11:12 -0500

    Create find mini value for a given combination of factors.sql

---
+++ find mini value for a given combination of factors.sql
@@ -0,0 +1,35 @@
+CREATE TABLE cars (
+  vin NUMBER(17) CONSTRAINT PK_cars PRIMARY KEY,
+  code NUMBER(2),
+  price NUMBER(6),
+  power NUMBER(4),
+);
+
+CREATE TABLE cars_property (
+  code NUMBER(2) CONSTRAINT PK_cars_property PRIMARY KEY,
+  mileage_range NUMBER(2),
+  is_clean_title NUMBER(1),
+);
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Question: Look for the cheapest clean title car for a given combination of mileage_range and power in used car markets
+
+Step1: Join tables to get master table with all the info
+Step2: Filter out not clean title cars
+Step3: Group by mileage_range and power, find the lowest price for a given combination of power and mileage_range
+Step4: Create a new view based on the above table and keep only cheapest cars at given combination of power and mileage_range
+Step5: Order new list by decending order of power and ascending order of mileage_range
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+SELECT vin, price, mileage_range, power
+FROM
+    (SELECT vin, mileage_range, price, power, MIN(price) OVER(PARTITION BY c.power, p.mileage_range ) miniprice 
+      FROM
+            cars c JOIN cars_property p ON c.code=p.code 
+      WHERE is_clean_title=1)
+WHERE price=miniprice
+ORDER BY power DESC, mileage_range ASC
+;
+
+
